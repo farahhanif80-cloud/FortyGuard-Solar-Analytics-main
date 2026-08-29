cc9d2899eb649797ddfe093f72ae5625aecfa422
farahhanif80 <farahhanif80@gmail.com>
2026-08-29 04:48:13 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import coverBg from "./assets/cover_bg.png";
 
 
 // e.g. "https://solarshield-backend.onrender.com"
-const BACKEND_URL = "http://127.0.0.1:8000";
+const BACKEND_URL = "fortyguard-solar-analytics-main-production.up.railway.app";
 
 export default function App() {
   const [rankedSites, setRankedSites] = useState([]);
